9058f4db411ec23f079a73ff45e43bc9cd2c2604
Theresa Shen <tshen@architech.ca>
2017-10-02 20:55:18 -0400

    Create a component and append it to the DOM to sat Hi

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component. This component should produce some HTML
+const App = function() { //const App = () => {}
+  return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create an instance of App, use <App></App> or <App />
+// Take this component's genrated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App/>, document.querySelector('.container'));
