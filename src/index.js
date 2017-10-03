37e985ef856be3b7df9ea4c77d726f4987e0c5bc
Theresa Shen <tshen@architech.ca>
2017-10-02 21:15:19 -0400

    Add Youtube Search API and Key

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyDdFHWlkcbT3wW3e_p_AlV-xHUYH8AcJGA';
+
 // Create a new component. This component should produce some HTML
 const App = function() { //const App = () => {}
   return <div>Hi!</div>;
